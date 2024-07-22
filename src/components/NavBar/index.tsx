96d6e5e9207b66e728ccac9664a238dfbef5ae34
John Romie <johnromie37@gmail.com>
2024-07-22 21:07:13 +0800

    chore: add spacing

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -41,7 +41,7 @@ const navigation = [
 const NavigationBar = () => {
   return (
     <Sheet>
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="container mx-auto px-4 pt-4 sm:px-6 lg:px-8">
         <div className="relative flex h-16 justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button */}
